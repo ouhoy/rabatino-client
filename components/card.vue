ffdd11a13a0efd8a93c88203ebb61805f9fb778f
Abdallah Dahmou <kimkinon@gmail.com>
2025-01-19 04:49:09 +0100

    Add scroll sections demo

---
+++ components/card.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+
+defineProps<{
+  title: string,
+  price: number,
+  imgUrl: string,
+}>()
+
+
+</script>
+
+<template>
+  <div class="w-full  flex flex-col gap-4 cursor-pointer ">
+
+    <!-- CARD TOP CONTAINER -->
+    <div class="relative w-full">
+
+      <!-- CARD TOP BUTTONS -->
+      <div class="absolute p-2 w-full flex items-center justify-between">
+        <div></div>
+<!--        <button class="bg-neutral-950 bg-opacity-45 p-1 rounded-full z-10 transform transition-transform duration-150 hover:scale-105">-->
+<!--          <HeartIcon class=" cursor-pointer h-6 w-6 text-white"/>-->
+<!--        </button>-->
+      </div>
+
+      <!-- IMG CONTAINER -->
+      <div style="width: 100%; padding-top: 66.66%; position: relative; overflow: hidden;">
+        <img loading="lazy" class="rounded-2xl object-cover bg-neutral-100"
+             style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" :src="imgUrl" :alt="title"/>
+      </div>
+
+    </div>
+
+    <!-- CARD DESCRIPTION -->
+    <div class="w-full flex items-center justify-between gap-2 min-w-80">
+      <h2 class="truncate  font-medium text-base text-neutral-900 first-letter:uppercase">{{ title }}</h2>
+      <p v-if="price" class="text-normal text-neutral-900 whitespace-nowrap"><span class="font-medium">{{ Math.floor(price) }}MAD</span> night</p>
+    </div>
+
+  </div>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import {MagnifyingGlassIcon} from '@heroicons/vue/16/solid';
 
 const searchInputValue = ref('');
 
@@ -8,12 +9,15 @@ const searchInputValue = ref('');
 
 <template>
   <main class="-w-full">
+
+    <!-- HERO_SECTION -->
     <section class="w-full flex flex-col items-center justify-center gap-4 py-36">
       <div class="flex text-center flex-col items-center justify-center gap-4 max-w-xl mx-auto">
         <h1 class="text-7xl text-neutral-900 leading-[80px] font-medium">Discover Rabat<br/>the City of Light</h1>
-        <p class="text-lg text-neutral-800">Explore the perfect blend of timeless heritage and cutting-edge innovation in Morocco’s capital.</p>
+        <p class="text-lg text-neutral-800">Explore the perfect blend of timeless heritage and cutting-edge innovation
+          in Morocco’s capital.</p>
       </div>
-      <div class="w-full py-2 h-14 border border-1 rounded-full border-neutral-300 bg-neutral-50/50 mx-auto  max-w-2xl">
+      <div class="w-full flex items-center justify-between p-2 h-14 border border-1 rounded-full border-neutral-300 bg-neutral-50/50 mx-auto  max-w-2xl">
 
         <input
 
@@ -23,6 +27,32 @@ const searchInputValue = ref('');
             type="text"
             class="bg-neutral-50 p-2 w-full max-w-[95%] mx-auto block outline-none border-0 text-neutral-900 placeholder:text-neutral-500 placeholder:text-sm"
             placeholder="What are you looking for?">
+
+        <button
+                class="p-3 bg-blue-600 flex items-center justify-center rounded-full ">
+          <MagnifyingGlassIcon class="w-4 h-4 text-white"/>
+        </button>
+      </div>
+    </section>
+
+    <section class="w-full">
+      <div class="w-full flex flex-col items-start justify-start gap-8">
+
+        <!-- EVENTS_HEADER -->
+        <div class="w-full flex items-start justify-between">
+
+          <div class="flex  flex-col items-start justify-start gap-2">
+            <h3 class="text-neutral-900 text-2xl font-semibold leading-none">Education</h3>
+            <p class="text-neutral-700 text-base font-normal leading-none">Anything related to education and studying</p>
+          </div>
+          <nuxt-link to="/" class="text-neutral-900 text-base font-medium">More education posts</nuxt-link>
+
+        </div>
+
+        <div class="w-full pl-28 flex items-center justify-center gap-8 overflow-x-scroll no-scrollbar">
+          <card v-for="item in Array.from({length:4})" title="The Ultimate Guide to Rabat" :price="0" imgUrl="https://images.unsplash.com/photo-1612838320302-3b3b3b3b3b3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjIwNzV8MHwxfGFsbHwxf"/>
+        </div>
+
       </div>
     </section>
   </main>
@@ -31,5 +61,14 @@ const searchInputValue = ref('');
 <style scoped>
 h1 {
   font-family: Alegreya;
+}
+
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+
+.no-scrollbar {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 }
 </style>
